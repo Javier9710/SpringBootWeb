673c3085172c33897fe397b84de73530a1d0b3a5
unknown <javierandresmd@ufps.edu.co>
2020-12-28 21:18:52 -0500

    enviar y recibir variables url con @PathVaribles

--- src/main/java/com/ufps/springboot/web/app/controllers/VariablesPathController.java
+++ src/main/java/com/ufps/springboot/web/app/controllers/VariablesPathController.java
@@ -1,6 +1,7 @@
 package com.ufps.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/variables")
 public class VariablesPathController {
 	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("titulo", "Enviar Parametros de la ruta(@PathVariable)");
+		return "variables/index";
+		
+		
+	}
+	
 	@GetMapping("/string/{texto}")
-	public String variables(PathVariable String texto) {
+	public String variables(@PathVariable String texto, Model model) {
 		
+		model.addAttribute("titulo", "Recibir Parametros de la ruta(@PathVariable)");
+		model.addAttribute("resultado", "El texto enviado en la ruta es: "+texto);
+		return "variables/ver";
+	}
+
+	@GetMapping("/string/{texto}/{numero}")
+	public String variables(@PathVariable String texto,@PathVariable int numero, Model model) {
 		
-		return "";
+		model.addAttribute("titulo", "Recibir Parametros de la ruta(@PathVariable)");
+		model.addAttribute("resultado", "El texto enviado en la ruta es: "+texto+
+				 " y el numero es: "+ numero);
+		return "variables/ver";
 	}
 
 }
